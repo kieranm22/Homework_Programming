bcb9be311cd0c77e08f44255023a916c24361a35
kieranm22 <makinikieran@gmail.com>
2025-02-22 13:53:18 -0500

    Homework 3

--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class PlayerDie : MonoBehaviour
 {
+    private string Enemy = "Enemy";
+    public GameObject endPanel;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Enemy)
+        {
+            endPanel.SetActive(true);
+            gameObject.SetActive(false);
+        }
+            
+                
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+
+
+    private float _xSpeed;
+    private float _ySpeed;
+
+    public float speed = 3;
+
+    private string Inputx = "Horizontal";
+    private string Inputy = "Vertical";
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(Inputx);
+        _ySpeed = Input.GetAxis(Inputy);
+
+        _rigidbody2D. velocity = new Vector2 (_xSpeed, _ySpeed) * speed;
+        
+    }
+
 
 }
+
+
